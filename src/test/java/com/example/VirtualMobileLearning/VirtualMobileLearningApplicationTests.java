631e51b6a3fe320faac97e91ef5ec182df64139a
ks0927 <brian_als@naver.com>
2022-10-10 13:55:11 +0900

    initial commit

---
+++ src/test/java/com/example/VirtualMobileLearning/VirtualMobileLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VirtualMobileLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualMobileLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
